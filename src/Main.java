556798f362d2c28365d87d49207f4cd399762331
mntsn <44491386+mntsn@users.noreply.github.com>
2018-11-05 19:49:44 +0100

    Inicio

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList <hotel> hoteles = new ArrayList<>();
+
+        System.out.println("¿Quiere introducir un nuevo hotel?");
+        System.out.println("Escriba 's' para sí y 'n' para no.");
+        Scanner sc = new Scanner(System.in);
+        String entrada = sc.next();
+        if (entrada=="s"){
+            hoteles.add ();
+            System.out.println("Introduzca el nombre");
+            String entradaNom= sc.next();
+
+        }
+    }
+}
