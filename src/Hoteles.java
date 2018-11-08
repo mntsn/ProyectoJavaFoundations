a87c8ecd29e2df96012194ab5a78c44f1ab12325
mntsn <44491386+mntsn@users.noreply.github.com>
2018-11-08 18:15:52 +0100

    Version 0.3

---
+++ src/Hoteles.java
@@ -0,0 +1,55 @@
+public class Hoteles {
+
+    private String nombre;
+    private int precio;
+    private boolean piscina;
+    private boolean wifi;
+    private boolean parking;
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombreHotel) {
+        if (nombreHotel==null||nombreHotel.isEmpty()){
+            System.out.println("El nombre no puede estar vacio.");
+        }
+        else {
+            nombre = nombreHotel;
+        }
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+    public void setPrecio(int precioHotel) {
+        if (precioHotel<0){
+            System.out.println("El precio debe ser positivo.");
+        }
+        else {
+            precio = precioHotel;
+        }
+    }
+
+    public boolean isPiscina() {
+        return piscina;
+    }
+    public void setPiscina(boolean piscina) {
+        this.piscina = piscina;
+    }
+
+    public boolean isWifi() {
+        return wifi;
+    }
+
+    public void setWifi(boolean wifi) {
+        this.wifi = wifi;
+    }
+
+    public boolean isParking() {
+        return parking;
+    }
+
+    public void setParking(boolean parking) {
+        this.parking = parking;
+    }
+}
